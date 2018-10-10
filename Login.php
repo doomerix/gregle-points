ca24870dc5a0f88b4f33305c296df7e2a9d8da81
Jetske <jdeboer891@student.novacollege.nl>
2018-10-10 11:03:34 +0200

    login is nog NIET af

--- Login.php
+++ Login.php
@@ -1,9 +1,57 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Include config file */
+require_once 'connection.php';
+//$_SESSION['user'] = 'random string';
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    //get user_id & password from post
+    $user_id = $_POST['user_id'];
+    $password = $_POST['password'];
 
-// hoi
+    $sql = "SELECT * FROM user WHERE user_id = ?";
+
+    $stmt = $link->prepare($sql);
+    $stmt->bind_param("s", $user_id);
+
+    $result = $stmt->get_result();
+
+    //given user_id can't be found, 0 records are returned/found
+    if ($result->num_rows === 0)
+    {
+        print 'Gebruiker niet gevonden';
+        exit;
+    }
+
+    $user = $result->fetch_assoc();
+
+    //when the query succeded and a racord has been found we will compare the password with the passwordhash from the database
+    if (password_verify($password, $user['PasswordHash']))
+    {
+        print 'aanmelden gelukt <br />';
+
+        $_SESSION['user'] = $user['user_id'];
+//        $_SESSION['isadmin'] = $user['IsAdmin']; //hoe ga ik dit maken zodat ik kan controleren wat voor rol de aangemelde gebruiker heeft?
+        /* Redirect to Index page */
+//        header('Location: '); //wat wordt de naam van de indexpagina en is er 1 index of zijn er 2 of 3 verschillende (voor elke rol een andere?)
+    }
+    else
+    {
+        print 'Aanmelden mislukt!';
+    }
+    $result->free();
+    $link->close();
+}
+else
+{
+    print 'Aanmelden mislukt!';
+}
+$result->free();
+$link->close();
+
+//if a logged-in user should nevertheless return to this login page, he/she will be sent back to the index page
+if (isset($_SESSION['user']))
+{
+//    header('Location: '); //wat wordt de naam van de indexpagina en is er 1 index of zijn er 2 of 3 verschillende (voor elke rol een andere?)
+}
+?>
