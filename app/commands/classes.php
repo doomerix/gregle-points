500e2fbd4827076d453615ff52fe91c023659d5d
doomerix <->
2018-11-14 11:45:52 +0100

    Re-implemented class management modal

--- app/commands/classes.php
+++ app/commands/classes.php
@@ -9,10 +9,23 @@ if (isset($_POST["addClass"])) {
     $insertClass = $connection->prepare("INSERT INTO class (class) VALUES (?) ;");
     $insertClass->bind_param("s", $className);
     $addClassState = $insertClass->execute();
+    $insertClass->free_result();
 }
 
 if (isset($_POST["deleteClass"])) {
     $classId = $_POST["deleteClass"];
+    $selectStudentsFromClass = $connection->prepare("SELECT studentnumber FROM student LEFT JOIN class ON class_id = class.id WHERE class = ? ;");
+    $selectStudentsFromClass->bind_param("s", $classId);
+    $selectStudentsFromClass->execute();
+    $selectStudentsResult = $selectStudentsFromClass->get_result();
+
+    while ($row = $selectStudentsResult->fetch_assoc()) {
+        $deleteUser = $connection->prepare("DELETE FROM user WHERE user_id = ? ;");
+        $deleteUser->bind_param("s", $row["studentnumber"]);
+        $deleteUser->execute();
+        $deleteUser->free_result();
+    }
+
     $deleteFromClass = $connection->prepare("DELETE FROM class WHERE class = ? ;");
     $deleteFromClass->bind_param("s", $classId);
     $deleteState = $deleteFromClass->execute();
@@ -47,7 +60,7 @@ if (isset($_POST["addClass"])) {
 $allClasses = $connection->query("SELECT id, class FROM class ORDER BY class ASC ;");
 ?>
     <div class="bodyWrap">
-        <div class="container col-sm-7">
+        <div class="container">
             <div>
                 <h2 class="paragraphMarginSmall">Klas toevoegen</h2>
                 <form method="post">
@@ -71,12 +84,49 @@ $allClasses = $connection->query("SELECT id, class FROM class ORDER BY class ASC
                     ?>
                     <div class="row justify-content-center pointsDiv">
                         <div class="col-5">
-                            <form method="post" style="margin-bottom:0;">
-                                <a href="?points_class=<?php echo $row["class"]; ?>"><span><?php echo "<b>" . $row["class"] . " </br></b><i>(" . $studentCount . " studenten)"; ?></i></span></a>
+                            <a href="?points_class=<?php echo $row["class"]; ?>"><span><?php echo "<b>" . $row["class"] . " </br></b><i>(" . $studentCount . " studenten)"; ?></i></span></a>
                         </div>
-                        <input type="hidden" name="deleteClass" value="<?php echo $row["class"] ?>">
-                        <button type="submit" class="btn btn-outline-danger">Verwijderen</button>
-                        </form>
+                        <button class="btn btn-outline-danger" data-toggle="modal"
+                                data-target="#<?php echo "modalclassremove" . $row["class"]; ?>">Verwijderen
+                        </button>
+                        <div class="modal fade" id="<?php echo "modalclassremove" . $row["class"]; ?>" tabindex="-1"
+                             role="dialog" aria-labelledby="<?php echo "modalclassremove" . $row["studentnumber"]; ?>"
+                             aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="<?php echo "modalclassremove" . $row["class"]; ?>">
+                                            Klas verwijderen?</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Weet je zeker dat je klas <?php echo $row["class"]; ?> wilt verwijderen?
+                                        <?php
+                                        if ($studentCount > 0) {
+                                            ?>
+                                            <br>
+                                            <b style="color: red">Als je een klas de verwijdert worden ook alle
+                                                <br>studenten die in klas <?php echo $row["class"]; ?> zitten
+                                                verwijdert!</b>
+
+                                            <?php
+                                        }
+                                        ?>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <form method="post">
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">
+                                                Annuleren
+                                            </button>
+                                            <input type="hidden" name="deleteClass" value="<?php echo $row["class"] ?>">
+                                            <button type="submit" class="btn btn-outline-danger">Verwijderen</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <hr>
                     <?php
